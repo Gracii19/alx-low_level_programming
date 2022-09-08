00f96a5a2d160aa20a4e4777dbab71bed38a82ed
Gracii19 <nobuhlegrace01@gmail.com>
2022-09-08 14:17:37 -0700

    Task 8 UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include <unistd.h>
+
 /**
- * main - Entry point
+ * main - prints to the standard error
  *
- * Return: 1
+ * Return: one
  */
 
 int main(void)
 
 {
 
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
 
