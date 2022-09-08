c3f3a5da1335e1840d845d8200d5d5637f81dade
Gracii19 <nobuhlegrace01@gmail.com>
2022-09-08 13:22:10 -0700

    Task 4 Hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *    * main - print to screen
- *
- *       *
- *
- *          * Return: zero
- *
- *             */
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	 char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts("\"Programming is like building a multilingual puzzle");
 
-
-
-	  puts(Mystr);
-
-	   return (0);
+	return (0);
 
 }
-
-
