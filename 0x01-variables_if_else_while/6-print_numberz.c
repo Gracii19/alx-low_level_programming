fc28ba90c53d2d2004e51d0290ac4698573b189c
Gracii19 <nobuhlegrace01@gmail.com>
2022-09-09 21:37:48 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,9 +3,9 @@
  /**
   * main - prints all single digit numbers of base 10 starting from 0
   *
-  * Description: using the main function
-  * this function prints all single digit numbers of base 10 starting from 0
-  * Return: Always 0.
+  *Description: using the main function
+  *this function prints all single digit numbers of base 10 starting from 0
+  *Return: Always 0.
   */
 int main(void)
 {
