086afff667213d3f17e905df9c55ade525b35cd7
Gracii19 <nobuhlegrace01@gmail.com>
2022-09-09 18:15:24 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - print alphabet in lowercase
+ * main - Entry point
  *
- *Return: zero
+ *Return: Always 0 (Success)
  */
 
 int main(void)
