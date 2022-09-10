890c8a497aa11c7dc81b20f08a01b518b78b1da2
Gracii19 <nobuhlegrace01@gmail.com>
2022-09-09 21:34:33 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
 
  /**
-  * main - Prints
+  * main - prints all single digit numbers of base 10 starting from 0
+  *
+  * Description: using the main function
+  * this function prints all single digit numbers of base 10 starting from 0
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+}
